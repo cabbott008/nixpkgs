be3618091163534f50a682e16fc6d6dac303c521
cabbott008 <77243923+cabbott008@users.noreply.github.com>
2025-01-07 19:16:01 -0600

    Update default.nix
    aurPatches link attempted repair

--- pkgs/os-specific/linux/nvidia-x11/default.nix
+++ pkgs/os-specific/linux/nvidia-x11/default.nix
@@ -142,25 +142,35 @@ rec {
   };
 
   # Last one supporting Kepler architecture
-  legacy_470 = generic {
-    version = "470.256.02";
-    sha256_64bit = "sha256-1kUYYt62lbsER/O3zWJo9z6BFowQ4sEFl/8/oBNJsd4=";
-    sha256_aarch64 = "sha256-e+QvE+S3Fv3JRqC9ZyxTSiCu8gJdZXSz10gF/EN6DY0=";
-    settingsSha256 = "sha256-kftQ4JB0iSlE8r/Ze/+UMnwLzn0nfQtqYXBj+t6Aguk=";
-    persistencedSha256 = "sha256-iYoSib9VEdwjOPBP1+Hx5wCIMhW8q8cCHu9PULWfnyQ=";
-    patches = [
-      "${aurPatches}/gcc-14.patch"
-      # fixes 6.10 follow_pfn
-      ./follow_pfn.patch
-      # https://gist.github.com/joanbm/a6d3f7f873a60dec0aa4a734c0f1d64e
-      (fetchpatch {
-        url = "https://gist.github.com/joanbm/a6d3f7f873a60dec0aa4a734c0f1d64e/raw/6bae5606c033b6c6c08233523091992370e357b7/nvidia-470xx-fix-linux-6.12.patch";
-        hash = "sha256-6nbzcRTRCxW8GDAhB8Zwx9rVcCzwPtVYlqoUhL9gxlY=";
-        stripLen = 1;
-        extraPrefix = "kernel/";
-      })
-    ];
-  };
+  legacy_470 = 
+    let
+      # Source corresponding to https://aur.archlinux.org/packages/nvidia-390xx-dkms
+      aurPatches = fetchgit {
+        url = "https://aur.archlinux.org/nvidia-390xx-utils.git";
+        rev = "ebb48c240ce329e89ad3b59e78c8c708f46f27b3";
+        hash = "sha256-AGx3/EQ81awBMs6rrXTGWJmyq+UjBCPp6/9z1BQBB9E=";
+      };
+
+    in
+      generic {
+        version = "470.256.02";
+        sha256_64bit = "sha256-1kUYYt62lbsER/O3zWJo9z6BFowQ4sEFl/8/oBNJsd4=";
+        sha256_aarch64 = "sha256-e+QvE+S3Fv3JRqC9ZyxTSiCu8gJdZXSz10gF/EN6DY0=";
+        settingsSha256 = "sha256-kftQ4JB0iSlE8r/Ze/+UMnwLzn0nfQtqYXBj+t6Aguk=";
+        persistencedSha256 = "sha256-iYoSib9VEdwjOPBP1+Hx5wCIMhW8q8cCHu9PULWfnyQ=";
+      patches = [
+        "${aurPatches}/gcc-14.patch"
+        # fixes 6.10 follow_pfn
+        ./follow_pfn.patch
+        # https://gist.github.com/joanbm/a6d3f7f873a60dec0aa4a734c0f1d64e
+        (fetchpatch {
+          url = "https://gist.github.com/joanbm/a6d3f7f873a60dec0aa4a734c0f1d64e/raw/6bae5606c033b6c6c08233523091992370e357b7/nvidia-470xx-fix-linux-6.12.patch";
+          hash = "sha256-6nbzcRTRCxW8GDAhB8Zwx9rVcCzwPtVYlqoUhL9gxlY=";
+          stripLen = 1;
+          extraPrefix = "kernel/";
+        })
+      ];
+    };
 
   # Last one supporting x86
   legacy_390 =
